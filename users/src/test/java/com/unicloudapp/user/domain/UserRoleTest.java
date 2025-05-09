68b26b64581744a87018ef49bec8ca2b4e074304
Michael <michalluczak4a@gmail.com>
2025-05-09 23:31:02 +0200

    refactor

--- users/src/test/java/com/unicloudapp/user/domain/UserRoleTest.java
+++ users/src/test/java/com/unicloudapp/user/domain/UserRoleTest.java
@@ -9,19 +9,12 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class UserRoleTest {
 
-    @Test
-    void shouldCreateUserRoleWithValidType() {
-        UserRole role = UserRole.of(UserRole.Type.ADMIN);
-
-        assertThat(role).isNotNull();
-        assertThat(role.getUserRoleType()).isEqualTo(UserRole.Type.ADMIN);
-    }
-
     @ParameterizedTest
     @EnumSource(UserRole.Type.class)
     void shouldCreateUserRoleForAllValidTypes(UserRole.Type type) {
         UserRole role = UserRole.of(type);
 
+        assertThat(role).isNotNull();
         assertThat(role.getUserRoleType()).isEqualTo(type);
     }
 
