a8b67f38f9d5611a2c9802254ad39f3c77c7c065
Michael <michalluczak4a@gmail.com>
2025-06-15 15:19:10 +0200

    fix pagination

--- users/src/test/groovy/com/unicloudapp/user/application/UserServiceSpec.groovy
+++ users/src/test/groovy/com/unicloudapp/user/application/UserServiceSpec.groovy
@@ -16,6 +16,7 @@ import com.unicloudapp.user.application.projection.UserFullNameProjection
 import com.unicloudapp.user.application.projection.UserRowProjection
 import com.unicloudapp.user.domain.User
 import com.unicloudapp.user.domain.UserFactory
+import org.springframework.data.domain.PageImpl
 import spock.lang.Specification
 import spock.lang.Subject
 import spock.lang.Title
@@ -254,7 +255,7 @@ class UserServiceSpec extends Specification {
         userRowProjection.getLastName() >> "Doe"
         userRowProjection.getEmail() >> "john.doe@example.com"
 
-        userRepository.findUserRowByIds(userIds, 0, 10) >> [userRowProjection]
+        userRepository.findUserRowByIds(userIds, 0, 10) >> new PageImpl([userRowProjection])
 
         when:
         def userDetails = userService.getUserDetailsByIds(userIds, 0, 10)
