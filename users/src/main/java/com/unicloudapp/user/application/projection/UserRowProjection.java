4901b531c3cd68ae978dd598dd701cdf718a9b9d
Michał Łuczak <contact@michal-luczak.pl>
2025-12-02 22:19:59 +0100

    UC-189 fix projection deserialization

--- users/src/main/java/com/unicloudapp/user/application/projection/UserRowProjection.java
+++ users/src/main/java/com/unicloudapp/user/application/projection/UserRowProjection.java
@@ -1,7 +1,9 @@
 package com.unicloudapp.user.application.projection;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.UUID;
 
+@JsonIgnoreProperties({"decoratedClass"})
 public interface UserRowProjection {
 
     UUID getUuid();
