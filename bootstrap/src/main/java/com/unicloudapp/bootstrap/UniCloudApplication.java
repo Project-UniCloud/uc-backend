9be432b6ca2efe2a8abd0167d88cbffa1205f6ac
Michał Łuczak <michalluczak4a@gmail.com>
2025-12-02 22:34:02 +0100

    UC-189 fix projection deserialization (#74)

--- bootstrap/src/main/java/com/unicloudapp/bootstrap/UniCloudApplication.java
+++ bootstrap/src/main/java/com/unicloudapp/bootstrap/UniCloudApplication.java
@@ -5,13 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication(scanBasePackages = "com.unicloudapp")
 @EnableJpaRepositories(basePackages = "com.unicloudapp")
 @EntityScan(basePackages = "com.unicloudapp")
 @ConfigurationPropertiesScan("com.unicloudapp")
 @EnableScheduling
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 class UniCloudApplication {
 
     static void main(String[] args) {
