2c1b67cc92fb3a46b4194e2a8340279f9bdce90f
Michał Łuczak <contact@michal-luczak.pl>
2025-12-11 00:23:02 +0100

    UC-206 change statistics endpoints format

--- cloud-management/src/main/java/com/unicloudapp/cloud/infrastructure/rest/CostStatisticsRestController.java
+++ cloud-management/src/main/java/com/unicloudapp/cloud/infrastructure/rest/CostStatisticsRestController.java
@@ -1,17 +1,13 @@
 package com.unicloudapp.cloud.infrastructure.rest;
 
 import com.unicloudapp.cloud.application.StatisticsService;
-import com.unicloudapp.common.group.GroupUniqueName;
-import com.unicloudapp.common.vo.cloud.CloudResourceType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/statistics/costs")
@@ -25,13 +21,13 @@ class CostStatisticsRestController {
         return ResponseEntity.ok(statisticsService.getTotalCosts());
     }
 
-    @GetMapping("per-resources-type")
-    ResponseEntity<Map<CloudResourceType, BigDecimal>> getOverallCostsPerResourceType() {
+    @GetMapping("/per-resources-type")
+    ResponseEntity<List<StatisticsService.CostPerResourceTypeDto>> getOverallCostsPerResourceType() {
         return ResponseEntity.ok(statisticsService.getOverallCostsPerResourceType());
     }
 
     @GetMapping("/per-group")
-    ResponseEntity<Map<GroupUniqueName, BigDecimal>> getTotalCostPerGroup() {
+    ResponseEntity<List<StatisticsService.CostPerGroupDto>> getTotalCostPerGroup() {
         return ResponseEntity.ok(statisticsService.getTotalCostPerGroup());
     }
 
