e12a593218dee3da55042251ea747571d77d8442
Michał Łuczak <contact@michal-luczak.pl>
2026-01-04 00:22:03 +0100

    UC-179 fixes

--- bootstrap/src/main/resources/db/changelog/1.0/0001-init-tables.sql
+++ bootstrap/src/main/resources/db/changelog/1.0/0001-init-tables.sql
@@ -11,25 +11,7 @@ CREATE TABLE IF NOT EXISTS cloud_resource_access_entity
     resource_type            VARCHAR(255)   NOT NULL,
     status                   VARCHAR(255)   NOT NULL,
     used_limit               numeric(38, 2) NOT NULL,
-    CONSTRAINT cloud_resource_access_entity_pkey PRIMARY KEY (cloud_resource_access_id)
-);
-
-CREATE TABLE IF NOT EXISTS group_cloud_resource_accesses
-(
-    group_id                  uuid NOT NULL,
-    cloud_resource_access_id  uuid NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS group_lecturers
-(
-    group_id uuid NOT NULL,
-    user_id  uuid
-);
-
-CREATE TABLE IF NOT EXISTS group_students
-(
-    group_id uuid NOT NULL,
-    user_id  uuid
+    CONSTRAINT pk_cloud_resource_access PRIMARY KEY (cloud_resource_access_id)
 );
 
 CREATE TABLE IF NOT EXISTS groups
@@ -41,33 +23,62 @@ CREATE TABLE IF NOT EXISTS groups
     name         VARCHAR(255) NOT NULL,
     semester     VARCHAR(255) NOT NULL,
     start_date   date,
-    CONSTRAINT groups_pkey PRIMARY KEY (uuid)
+    CONSTRAINT pk_groups PRIMARY KEY (uuid),
+    CONSTRAINT uk_groups_name_semester UNIQUE (name, semester)
 );
 
 CREATE TABLE IF NOT EXISTS users
 (
-    uuid       uuid         NOT NULL,
-    email      VARCHAR(255),
-    first_name VARCHAR(255) NOT NULL,
-    last_login TIMESTAMP    WITHOUT TIME ZONE,
-    last_name  VARCHAR(255) NOT NULL,
-    login      VARCHAR(255) NOT NULL,
-    role       VARCHAR(255) NOT NULL,
-    CONSTRAINT users_pkey PRIMARY KEY (uuid)
+    uuid        uuid         NOT NULL,
+    email       VARCHAR(255),
+    first_name  VARCHAR(255) NOT NULL,
+    last_login  TIMESTAMP WITHOUT TIME ZONE,
+    last_name   VARCHAR(255) NOT NULL,
+    login       VARCHAR(255) NOT NULL,
+    CONSTRAINT  pk_users PRIMARY KEY (uuid),
+    CONSTRAINT  uk_users_login UNIQUE (login)
 );
 
-ALTER TABLE groups
-    ADD CONSTRAINT uk75amegq7vb6lfkh7ofpgvbtyf UNIQUE (name, semester);
+CREATE TABLE IF NOT EXISTS user_roles
+(
+    user_uuid   uuid         NOT NULL,
+    role        VARCHAR(255) NOT NULL,
+    PRIMARY KEY (user_uuid, role),
+    CONSTRAINT fk_ur_users
+        FOREIGN KEY (user_uuid)
+            REFERENCES users (uuid)
+);
 
-ALTER TABLE users
-    ADD CONSTRAINT ukow0gan20590jrb00upg3va2fn UNIQUE (login);
+CREATE TABLE IF NOT EXISTS group_cloud_resource_accesses
+(
+    group_id                 uuid NOT NULL,
+    cloud_resource_access_id uuid NOT NULL,
+    CONSTRAINT pk_group_cloud_resource_accesses PRIMARY KEY (group_id, cloud_resource_access_id),
+    CONSTRAINT fk_gcra_group
+        FOREIGN KEY (group_id) REFERENCES groups (uuid),
+    CONSTRAINT fk_gcra_cloud_resource_access
+        FOREIGN KEY (cloud_resource_access_id)
+            REFERENCES cloud_resource_access_entity (cloud_resource_access_id)
+);
 
-ALTER TABLE group_students
-    ADD CONSTRAINT fk5xwqsk30pbk3opg7gyuqydfua FOREIGN KEY (group_id) REFERENCES groups (uuid) ON DELETE NO ACTION;
+CREATE TABLE IF NOT EXISTS group_lecturers
+(
+    group_id uuid NOT NULL,
+    user_id  uuid NOT NULL,
+    CONSTRAINT pk_group_lecturers PRIMARY KEY (group_id, user_id),
+    CONSTRAINT fk_group_lecturers_group
+        FOREIGN KEY (group_id) REFERENCES groups (uuid),
+    CONSTRAINT fk_group_lecturers_user
+        FOREIGN KEY (user_id) REFERENCES users (uuid)
+);
 
-ALTER TABLE group_lecturers
-    ADD CONSTRAINT fk6cogcp6nqeicoe4778gtr7stb FOREIGN KEY (group_id) REFERENCES groups (uuid) ON DELETE NO ACTION;
-
-ALTER TABLE group_cloud_resource_accesses
-    ADD CONSTRAINT fkh49j08oinmat190qeeo5uddcn FOREIGN KEY (group_id) REFERENCES groups (uuid) ON DELETE NO ACTION;
-
+CREATE TABLE IF NOT EXISTS group_students
+(
+    group_id uuid NOT NULL,
+    user_id  uuid NOT NULL,
+    CONSTRAINT pk_group_students PRIMARY KEY (group_id, user_id),
+    CONSTRAINT fk_group_students_group
+        FOREIGN KEY (group_id) REFERENCES groups (uuid),
+    CONSTRAINT fk_group_students_user
+        FOREIGN KEY (user_id) REFERENCES users (uuid)
+);
